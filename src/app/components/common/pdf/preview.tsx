66ca77abb41b103add80f60286ee0eb98c64c96d
SUNHEE PARK <sunheepark@SUNHEEui-MacBookPro.local>
2024-03-11 00:23:55 +0900

    5. PDF 미리보기 페이지네이션 구현

--- src/app/components/common/pdf/preview.tsx
+++ src/app/components/common/pdf/preview.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef} from 'react';
+import { Pagination } from 'antd';
 import * as pdfjs from 'pdfjs-dist';
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 
@@ -8,51 +9,70 @@ interface PDFViewerProps {
 }
 
 const PDFPreviewComponent: React.FC<PDFViewerProps> = ({ pdfUrl }) => {
-  pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+    pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
   
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+    const [numPages, setNumPages] = useState<number>(1);
+    const [pageNumber, setPageNumber] = useState<number>(1);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    // PDF 파일 로드
-    pdfjs.getDocument(pdfUrl)
-      .promise.then(pdf => {
-        // 첫 번째 페이지 가져오기
-        return pdf.getPage(1);
-      })
-      .then(page => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
-
-        // 캔버스에 렌더링
-        const viewport = page.getViewport({ scale: 1 });
-        const context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        const renderContext = {
-          canvasContext: context!,
-          viewport: viewport
+    useEffect(() => {
+        // PDF 문서의 페이지 수를 가져오는 함수
+        const fetchNumPages = async () => {
+            try {
+                const pdf = await pdfjs.getDocument(pdfUrl).promise;
+                setNumPages(pdf.numPages * 10);
+            } catch (error) {
+                console.error('Error fetching PDF:', error);
+            }
         };
-        return page.render(renderContext).promise;
-      })
-      .then(() => {
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error('PDF 파일 로드 오류:', error);
-      });
-  }, []);
+        
+        fetchNumPages();
+    }, [pdfUrl]);
 
-  return (
-    <div style={{ margin: '20px 50px' }}>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <canvas ref={canvasRef}></canvas>
-      )}
-    </div>
-  );
+    useEffect(() => {
+        // 선택된 페이지를 Canvas에 렌더링하는 함수
+        const renderPage = async () => {
+            try {
+                const pdf = await pdfjs.getDocument(pdfUrl).promise;
+                const page = await pdf.getPage(pageNumber);
+                const canvas = canvasRef.current!;
+
+                if (!canvas) {
+                    console.log("canvas error");
+                    return;
+                }
+
+                const context = canvas.getContext('2d')!;
+                const viewport = page.getViewport({ scale: 1.5 });
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                const renderContext = {
+                    canvasContext: context,
+                    viewport: viewport,
+                };
+                await page.render(renderContext).promise;
+            } catch (error) {
+                console.error('Error rendering PDF page:', error);
+            }
+        };
+
+        renderPage();
+    }, [pdfUrl, pageNumber]);
+
+    // 페이지 변경 이벤트 핸들러
+    const onPageChange = (page: number) => {
+        setPageNumber(page);
+    };
+
+    return (
+        <div style={{ margin: '20px 50px' }}>
+            {/* Canvas 요소를 사용하여 PDF 렌더링 */}
+            <canvas ref={canvasRef} />
+            {/* 페이지네이션 UI */}
+            <Pagination current={pageNumber} onChange={onPageChange} total={numPages} />
+        </div>
+    );
 };
 
 export default PDFPreviewComponent;
