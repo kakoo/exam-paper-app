75d587a4fdc34ca3109647874353775703fb0ea9
SUNHEE PARK <sunheepark@SUNHEEui-MacBookPro.local>
2024-03-11 09:52:59 +0900

    6. 시험지 정보 업데이트

--- src/app/components/exam/item.tsx
+++ src/app/components/exam/item.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useSearchParams } from 'next/navigation';
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from 'react';
 import type { RadioChangeEvent } from 'antd';
 import { Layout, Radio, Button, Form, Input, message, Space } from 'antd';
@@ -16,23 +17,20 @@ interface Item {
 const ExamItemComponent = () => {
   const [form] = Form.useForm();
   const searchParams = useSearchParams();
-  const id = searchParams.get('id');
-
+  const [loading, setLoading] = useState(false);
   const [examItem, setExamData] = useState<any>(null);
   const [isVisible, setIsVisible] = useState(false);
+  const router = useRouter();
 
-  const onFinish = () => {
-    message.success('Submit success!');
-  };
-  
+  const id = searchParams.get('id');
+  const url = `/api/exam-paper/${id}?participant=${process.env.NEXT_PUBLIC_API_ID}`;
+
   const onFinishFailed = () => {
-    message.error('Submit failed!');
+    message.error('시험지 정보를 확인해주세요.');
   };
   
   const onList = () => {
-     form.setFieldsValue({
-       url: '/',
-     });
+    router.push("/");
   };
 
   const onChangeShow = (e: RadioChangeEvent) => {
@@ -40,6 +38,31 @@ const ExamItemComponent = () => {
     setIsVisible(e.target.value);
   };
 
+  const onFinish = async (values: any) => {
+    setLoading(true);
+    try {
+        values.isVisible = isVisible;
+        console.log(values);
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(values), // 사용자가 입력한 데이터를 JSON 형식으로 변환하여 body에 넣음
+        });
+
+        // 응답 확인
+        if (response.ok) {
+            console.log('Data updated successfully!');
+        } else {
+            console.error('Failed to update data:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error updating data:', error);
+    } finally {
+        setLoading(false);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -77,16 +100,16 @@ const ExamItemComponent = () => {
       <Form.Item
         name="title"
         label="시험지 제목"
-        rules={[{ required: true }, { type: 'string', min: 2 }]}
+        rules={[{ required: true, message: '제목을 입력하세요.' }]}
       >
-        <Input defaultValue={examItem.title} />
+        <Input defaultValue={examItem.title} value={examItem.title} />
       </Form.Item>
       <Form.Item
         name="highSchoolYear"
         label="학년"
-        rules={[{ required: true }, { type: 'number', min: 1 }]}
+        rules={[{ required: true, message: '학년을 입력하세요.' }]}
       >
-        <Input defaultValue={examItem.highSchoolYear} />
+        <Input defaultValue={examItem.highSchoolYear} value={examItem.highSchoolYear} />
       </Form.Item>
       <Form.Item label="공개여부">
           <Radio.Group onChange={onChangeShow} value={isVisible}>
@@ -96,7 +119,7 @@ const ExamItemComponent = () => {
         </Form.Item>
       <Form.Item>
         <Space>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" htmlType="submit" loading={loading}>
             수정
           </Button>
           <Button htmlType="button" onClick={onList}>
